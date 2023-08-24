81182d4faf5935aa57db52cad4b240ed7444df37
Z3R0O0O <127508635+Z3R0O0O@users.noreply.github.com>
2023-08-24 16:24:35 -0300

    scaner.py

--- scaner.py
+++ scaner.py
@@ -7,37 +7,39 @@ Eduardo do Nascimento Silva, RM99225
 Gustavo Duarte Bezerra da Silva, RM99774
 Henrique Batista de Souza, RM99742
 '''
-# pip install nmap
-import nmap
 
-def scan_ports(host):                           # Chama o nmap para verificar da porta 1 a 65535
-    scanner = nmap.PortScanner()                # Escaneia as portas
-    scanner.scan(host, arguments='-p 1-65535')  # Da 1ª á 65.535ª
-    return scanner[host]['tcp'].keys()
+import socket
 
-def save_results(filename, resultados):         
+def scan_ports(host, ports):
+    portas_abertas = []  # Criação de uma lista vazia que armazenara as portas abertas encontradas durante o escaneamento.
+    for port in ports:   # Inicia um loop que percorrerá cada número de porta na lista
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #Cria um objeto de socket usando IPv4 e o tipo de socket para fluxos de dados TCP 
+        sock.settimeout(1)  # Definindo um timeout para a conexão
+        result = sock.connect_ex((host, port))
+        sock.close()
+        if result == 0:     # Verifica se a conexão foi estabelecida com sucesso. Se sim, adiciona o número da porta à lista portas_abertas.
+            portas_abertas.append(port)
+    return portas_abertas
+
+def save_results(filename, results):
     with open(filename, 'w') as file:
-        for porta in resultados:
-            file.write(f"Porta {porta}: Aberta\n")
+        for portas in results:
+            file.write(f"Porta {portas}: Aberta\n")
 
 def main():
-    maquina = "127.0.0.1"                       # IP da máquina
+    ip_alvo = "127.0.0.1"           # IP onde haverá o escaneamento
+    portas_alvos = range(1, 65536)  # Todas as portas de 1 a 65535
     
-    portas_abertas = scan_ports(maquina)        # Para salvar o scan em .txt
-    save_results("resultado_do_scan.txt", portas_abertas)
+    portas_abertas = scan_ports(ip_alvo, portas_alvos)    # Realizado o escaneamento das portas no IP especificado. O resultado é armazenado na variável portas_abertas
+    save_results("resultados.txt", portas_abertas)        # Vê quais portas estão abertas e armazena em um .txt
     
-    fiware = [1026, 1883, 4041, 8666, 27017]    # Portas para verificar 
+    portas_fiware = [1026, 1883, 4041, 8666, 27017]            # Portas que o Fiware abre
     
-    fiware_encontrado = False                   # Caso encontre as portas abertas
-    for portas in fiware:
-        if portas in portas_abertas:
-            fiware_encontrado = True
-            break
-    
-    if fiware_encontrado:                       # Avisa se foi encontrado ou não
-        print("Fiware em funcionamento")
+    fiware_encontrado = any(port in portas_abertas for port in portas_fiware)    # verifica se algum dos números de porta Fiware está presente na lista de portas abertas
+    if fiware_encontrado:
+        print("Fiware rodando aqui")
     else:
-        print("Fiware desligado")
+        print("Nenhum fiware encontrado")
 
 if __name__ == "__main__":
     main()
